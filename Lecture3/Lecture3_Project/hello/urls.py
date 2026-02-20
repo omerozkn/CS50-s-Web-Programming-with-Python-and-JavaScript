fde81bd042d1540af8adbf287e44fb80e6b12a44
Ömer <omerfarukozkan08@gmail.com>
2026-02-20 15:00:58 +0300

    something, something

--- Lecture3/Lecture3_Project/hello/urls.py
+++ Lecture3/Lecture3_Project/hello/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns =[
-    path ("", views.index, name= "index")
+    path ("<str:name>", views.greet, name="greet"),
+    path ("", views.index, name= "index"),
+    path ("lydia", views.lydia, name= "lydia")
+    
 ]
--- Lecture3/Lecture3_Project/hello/views.py
+++ Lecture3/Lecture3_Project/hello/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def index(reguest):
     return HttpResponse("Hello, world!")
+def lydia(request):
+    return HttpResponse("long live lydia")
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
